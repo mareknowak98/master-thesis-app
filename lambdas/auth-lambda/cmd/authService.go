1d9ddf8e999cdc2cdc24686dbd90f992807564c3
Marek <mark-now@wp.pl>
2022-06-27 14:05:13 +0200

    authorizer lambda added

--- lambdas/auth-lambda/cmd/authService.go
+++ lambdas/auth-lambda/cmd/authService.go
@@ -1,22 +1,29 @@
 package cmd
 
 import (
-	"regexp"
+	"strings"
 )
 
-// function return bool value if given group is authorized for exact request
-// data is stored in hash map for better performance (comparing e.g. to read from json file)
-//func isAllowed(userGroup string, method string) bool {
-//	permissions := map[string][]string{
-//		"student-group": []string{},
-//		"teacher-group": []string{},
-//	}
-//	return false
-//}
+// IsAllowed function return bool value if given group is authorized for exact request
+//data is stored in hash map for better performance (comparing e.g. to read from json file)
+func IsAllowed(method string, userGroup string) bool {
+	permissions := map[string]map[string]bool{
+		"GET/grades":  map[string]bool{"teacher-group": true, "student-group": true},
+		"POST/grades": map[string]bool{"teacher-group": true},
+	}
 
+	if endpointPerm, ok := permissions[method]; ok {
+		if isAllowed, ok := endpointPerm[userGroup]; ok {
+			return isAllowed
+		}
+	}
+
+	return false
+}
+
+// MethodArnToUrl Function get url from Method arn
+// arn:aws:execute-api:eu-central-1:614695401085:y1llo92zmi/test/GET/grades -> GET/grades
 func MethodArnToUrl(methodArn string) string {
-	// TODO: to finish
-	var validID = regexp.MustCompile(`/^(?:[^\/]*\/){2}\s*/`)
-
-	return validID.FindString(methodArn)
+	var validID = strings.Split(methodArn, "/")[2:]
+	return strings.Join(validID, "/")
 }
--- lambdas/auth-lambda/main.go
+++ lambdas/auth-lambda/main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/golang-jwt/jwt/v4"
 	"mylearnproject/lambdas/auth-lambda/cmd"
 )
 
+// HandleRequest Handle lambda request (from api gateway)
 func HandleRequest(ctx context.Context, request events.APIGatewayV2CustomAuthorizerV1Request) (events.APIGatewayCustomAuthorizerResponse, error) {
-	//var res events.APIGatewayCustomAuthorizerResponse
-	fmt.Printf("url: %#v\n", cmd.MethodArnToUrl("arn:aws:execute-api:eu-central-1:614695401085:y1llo92zmi/test/GET/grades"))
-
-	fmt.Printf("Request: %#v\n", request)
-
+	endpointUrl := cmd.MethodArnToUrl(request.MethodArn)
 	parsedToken, err := cmd.DecodeToken(request.AuthorizationToken)
+
+	// Return deny if token cannot be parsed
 	if err != nil {
 		return authPolicyResponse("user", "Deny", request.MethodArn, nil), nil
 	}
 
-	// check if claims are present and token is valid.
+	//get user group from token claims, then map []interface {} to string
+	userGroup := parsedToken.Claims.(jwt.MapClaims)["cognito:groups"].([]interface{})[0].(string)
+	isAllowed := cmd.IsAllowed(endpointUrl, userGroup)
+
+	// Return deny if user has no permission to access endpoint (based on user group)
+	if !isAllowed {
+		return authPolicyResponse("user", "Deny", request.MethodArn, nil), nil
+	}
+
+	// Check if claims are present and token is valid.
 	if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
 		// return Allow authResponse with userEntity in authorizer context for next lambda in chain.
 		return authPolicyResponse("user", "Allow", request.MethodArn, map[string]interface{}{"userEntity": claims["sub"].(string)}), nil
@@ -30,6 +37,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayV2CustomAuthori
 }
 
 // authPolicyResponse for authorization/ de-authorization of given principal with supplied context
+// Used as built-in map for better performance
 func authPolicyResponse(principalID, effect, resource string, context map[string]interface{}) events.APIGatewayCustomAuthorizerResponse {
 	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalID}
 	if effect != "" && resource != "" {
