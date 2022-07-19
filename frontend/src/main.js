b93fbaa572f3769bc7111279cda82625db44d6f2
Marek <mark-now@wp.pl>
2022-07-19 16:41:56 +0200

    primevue init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Ripple from "primevue/ripple"; //icons
+
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(PrimeVue, { ripple: true });
+
+// eslint-disable-next-line vue/multi-word-component-names
+app.component("Button", Button);
+app.directive("Ripple", Ripple);
+
+app.mount("#app");
