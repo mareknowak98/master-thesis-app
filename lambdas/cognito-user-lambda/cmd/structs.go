ffe998da5827883e34b2fab42b6f4db4bbc10e72
Marek <mark-now@wp.pl>
2022-07-20 18:43:27 +0200

    login lambda output changed

--- lambdas/cognito-user-lambda/cmd/structs.go
+++ lambdas/cognito-user-lambda/cmd/structs.go
@@ -4,3 +4,8 @@ type UserLogin struct {
 	Username string `json:"USERNAME"`
 	Password string `json:"PASSWORD"`
 }
+
+type UserLoginResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
