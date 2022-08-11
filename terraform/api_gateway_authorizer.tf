78e5891fb7db345f1660c792560b69d2c99f0dfd
Marek <mark-now@wp.pl>
2022-08-12 01:09:28 +0200

    cors in autrhorizer

--- terraform/api_gateway_authorizer.tf
+++ terraform/api_gateway_authorizer.tf
@@ -5,6 +5,18 @@ resource "aws_api_gateway_authorizer" "mylearn" {
   authorizer_credentials = aws_iam_role.mylearn_invocation_role.arn
 }
 
+resource "aws_api_gateway_gateway_response" "mylearn_authorizer" {
+  rest_api_id   = aws_api_gateway_rest_api.mylearn.id
+  status_code   = "401"
+  response_type = "DEFAULT_4XX"
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin": "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers": "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "gatewayresponse.header.Access-Control-Allow-Methods": "'GET,POST,OPTIONS'"
+  }
+}
+
 resource "aws_iam_role" "mylearn_invocation_role" {
   name = "api_gateway_auth_invocation"
   path = "/"
