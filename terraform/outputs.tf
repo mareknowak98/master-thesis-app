7d0eacfb07c46bfed262d61243d4d2463ddae384
Marek <mark-now@wp.pl>
2022-09-06 22:50:14 +0200

    tf uncomment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,14 @@ output "websocket_chat_api_url" {
   value = aws_apigatewayv2_stage.chat_api.invoke_url
 }
 
+output "websocket_lesson_api_url" {
+  value = aws_apigatewayv2_stage.mylearn_lessons_api.invoke_url
+}
+
+output "s3_api_url" {
+  value = aws_api_gateway_deployment.mylearn_s3.invoke_url
+}
+
 #output "cloudfront_url" {
 #  value = aws_cloudfront_distribution.mylearn.domain_name
 #}
